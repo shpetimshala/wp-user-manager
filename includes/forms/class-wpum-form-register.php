b3026c76590eac57eb46c6745967af1320a162ce
Alessandro Tesoro <mythemesdepot@gmail.com>
2015-02-22 12:55:27 +0000

    prototype submitted registration fields data storage

--- includes/forms/class-wpum-form-register.php
+++ includes/forms/class-wpum-form-register.php
@@ -34,6 +34,106 @@ class WPUM_Form_Register extends WPUM_Form {
 	}
 
 	/**
+	 * Define registration fields
+	 *
+	 * @access public
+	 * @since 1.0.0
+	 * @return void
+	 */
+	public static function get_registration_fields() {
+
+		if ( self::$fields ) {
+			return;
+		}
+
+		self::$fields = apply_filters( 'wpum_default_registration_fields', array(
+			'register' => array(
+				'username' => array(
+					'label'       => __( 'Username' ),
+					'type'        => 'text',
+					'required'    => true,
+					'placeholder' => '',
+					'priority'    => 1
+				),
+				'email' => array(
+					'label'       => __( 'Email' ),
+					'type'        => 'email',
+					'required'    => true,
+					'placeholder' => '',
+					'priority'    => 2
+				),
+			),
+		) );
+
+	}
+
+	/**
+	 * Get submitted fields values.
+	 *
+	 * @access public
+	 * @since 1.0.0
+	 * @return $values array of data from the fields.
+	 */
+	protected static function get_posted_fields() {
+
+		// Get fields
+		self::get_registration_fields();
+
+		$values = array();
+
+		foreach ( self::$fields as $group_key => $group_fields ) {
+			foreach ( $group_fields as $key => $field ) {
+				// Get the value
+				$field_type = str_replace( '-', '_', $field['type'] );
+
+				if ( method_exists( __CLASS__, "get_posted_{$field_type}_field" ) ) {
+					$values[ $group_key ][ $key ] = call_user_func( __CLASS__ . "::get_posted_{$field_type}_field", $key, $field );
+				} else {
+					$values[ $group_key ][ $key ] = self::get_posted_field( $key, $field );
+				}
+
+				// Set fields value
+				self::$fields[ $group_key ][ $key ]['value'] = $values[ $group_key ][ $key ];
+			}
+		}
+
+		return $values;
+	}
+
+	/**
+	 * Goes through fields and sanitizes them.
+	 *
+	 * @access public
+	 * @param array|string $value The array or string to be sanitized.
+	 * @since 1.0.0
+	 * @return array|string $value The sanitized array (or string from the callback)
+	 */
+	public static function sanitize_posted_field( $value ) {
+		// Decode URLs
+		if ( is_string( $value ) && ( strstr( $value, 'http:' ) || strstr( $value, 'https:' ) ) ) {
+			$value = urldecode( $value );
+		}
+
+		// Santize value
+		$value = is_array( $value ) ? array_map( array( __CLASS__, 'sanitize_posted_field' ), $value ) : sanitize_text_field( stripslashes( trim( $value ) ) );
+
+		return $value;
+	}
+
+	/**
+	 * Get the value of submitted fields.
+	 *
+	 * @access protected
+	 * @param  string $key
+	 * @param  array $field
+	 * @since 1.0.0
+	 * @return array|string content of the submitted field
+	 */
+	protected static function get_posted_field( $key, $field ) {
+		return isset( $_POST[ $key ] ) ? self::sanitize_posted_field( $_POST[ $key ] ) : '';
+	}
+
+	/**
 	 * Process the submission.
 	 *
 	 * @access public
@@ -42,7 +142,17 @@ class WPUM_Form_Register extends WPUM_Form {
 	 */
 	public static function process() {
 		
-		echo "string";
+		// Get fields
+		self::get_registration_fields();
+
+		// Get posted values
+		$values = self::get_posted_fields();
+
+		if ( empty( $_POST['wpum_submit_form'] ) ) {
+			return;
+		}
+
+		print_r($values);
 
 	}
 
@@ -55,10 +165,15 @@ class WPUM_Form_Register extends WPUM_Form {
 	 */
 	public static function output( $atts = array() ) {
 		
+		// Get fields
+		self::get_registration_fields();
+
+		// Display template
 		get_wpum_template( 'default-registration-form.php', 
 			array(
 				'atts' => $atts,
-				'form' => self::$form_name
+				'form' => self::$form_name,
+				'register_fields' => self::get_fields( 'register' ),
 			)
 		);
 
--- templates/default-registration-form.php
+++ templates/default-registration-form.php
@@ -9,20 +9,32 @@
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       1.0.0
  */
-
 ?>
 <div id="wpum-form-register-<?php echo $atts['form_id'];?>" class="wpum-default-registration-form-wrapper" data-redirect="<?php echo $atts['redirect'];?>">
 
 	<?php do_action( 'wpum_before_default_register_form_template', $atts ); ?>
 
-	<form action="register" method="post" id="wpum-register-<?php echo $atts['form_id'];?>" class="wpum-default-registration-form" name="wpum-register-<?php echo $atts['form_id'];?>">
+	<form action="" method="post" id="wpum-register-<?php echo $atts['form_id'];?>" class="wpum-default-registration-form" name="wpum-register-<?php echo $atts['form_id'];?>">
 
 		<?php do_action( 'wpum_before_inside_default_register_form_template', $atts ); ?>
-		Testing Form
+
+		<?php foreach ( $register_fields as $key => $field ) : ?>
+			<fieldset class="fieldset-<?php esc_attr_e( $key ); ?>">
+				<label for="<?php esc_attr_e( $key ); ?>"><?php echo $field['label']; ?></label>
+				<div class="field <?php echo $field['required'] ? 'required-field' : ''; ?>">
+					<?php get_wpum_template( 'form-fields/' . $field['type'] . '-field.php', array( 'key' => $key, 'field' => $field ) ); ?>
+				</div>
+			</fieldset>
+		<?php endforeach; ?>
+
 		<?php do_action( 'wpum_after_inside_default_register_form_template', $atts ); ?>
 
-		<input type="hidden" name="wpum_submit_form" value="<?php echo $form; ?>" />
-		<input type="submit" name="submit_job" class="button" value="Submit" />
+		<?php wp_nonce_field( 'wpum-register-nonce', 'security' ); ?>
+
+		<p>
+			<input type="hidden" name="wpum_submit_form" value="<?php echo $form; ?>" />
+			<input type="submit" name="submit_wpum_register" class="button" value="<?php _e('Register'); ?>" />
+		</p>
 
 	</form>
 
