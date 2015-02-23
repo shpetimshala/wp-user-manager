a0f938d9a4fd659a5387e35d615bb4bd3e090412
Alessandro Tesoro <mythemesdepot@gmail.com>
2015-02-23 00:20:40 +0000

    added notification message for correct configuration

--- includes/admin/admin-notices.php
+++ includes/admin/admin-notices.php
@@ -21,6 +21,10 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 function wpum_admin_messages() {
 	global $wpum_options;
 
+	if (  isset( $_GET['settings-updated'] ) && $_GET['settings-updated'] == true && !wpum_get_option('custom_passwords') && wpum_get_option('password_strength') ) {
+		add_settings_error( 'wpum-notices', 'custom-passwords-disabled', __( 'You have enabled the "Minimum Password Strength" option, the "Users custom passwords" is currently disabled and must be enabled for custom passwords to work.', 'wpum' ), 'error' );
+	}
+
 	settings_errors( 'wpum-notices' );
 }
 add_action( 'admin_notices', 'wpum_admin_messages' );
