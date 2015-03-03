c30d6fdde4d92b4ab8ee3fba1672663b53b621b8
Alessandro Tesoro <mythemesdepot@gmail.com>
2015-03-03 15:27:10 +0000

    prototype checking if user exists in psw recovery form

--- includes/forms/class-wpum-form-password.php
+++ includes/forms/class-wpum-form-password.php
@@ -31,6 +31,9 @@ class WPUM_Form_Password extends WPUM_Form {
 
 		add_action( 'wp', array( __CLASS__, 'process' ) );
 
+		// Validate username field
+		add_filter( 'wpum_password_form_validate_fields', array( __CLASS__, 'validate_username' ), 10, 3 );
+
 	}
 
 	/**
@@ -47,6 +50,15 @@ class WPUM_Form_Password extends WPUM_Form {
 		}
 
 		self::$fields = apply_filters( 'wpum_password_fields', array(
+			'user' => array(
+				'username_email' => array(
+					'label'       => __( 'Username or email' ),
+					'type'        => 'text',
+					'required'    => true,
+					'placeholder' => '',
+					'priority'    => 1
+				),
+			),
 			'password' => array(
 				'password_1' => array(
 					'label'       => __( 'Password' ),
@@ -65,6 +77,115 @@ class WPUM_Form_Password extends WPUM_Form {
 			),
 		) );
 
+		// Temporarily remove fields if not into password reset form
+		if( !isset( $_GET['password-reset'] ) ) :
+			unset( self::$fields['password']['password_1'] );
+			unset( self::$fields['password']['password_2'] );
+		endif;
+
+	}
+
+	/**
+	 * Get submitted fields values.
+	 *
+	 * @access public
+	 * @since 1.0.0
+	 * @return $values array of data from the fields.
+	 */
+	protected static function get_posted_fields() {
+
+		// Get fields
+		self::get_password_fields();
+
+		$values = array();
+
+		foreach ( self::$fields as $group_key => $group_fields ) {
+			foreach ( $group_fields as $key => $field ) {
+				// Get the value
+				$field_type = str_replace( '-', '_', $field['type'] );
+
+				if ( method_exists( __CLASS__, "get_posted_{$field_type}_field" ) ) {
+					$values[ $group_key ][ $key ] = call_user_func( __CLASS__ . "::get_posted_{$field_type}_field", $key, $field );
+				} else {
+					$values[ $group_key ][ $key ] = self::get_posted_field( $key, $field );
+				}
+
+				// Set fields value
+				self::$fields[ $group_key ][ $key ]['value'] = $values[ $group_key ][ $key ];
+			}
+		}
+
+		return $values;
+	}
+
+	/**
+	 * Goes through fields and sanitizes them.
+	 *
+	 * @access public
+	 * @param array|string $value The array or string to be sanitized.
+	 * @since 1.0.0
+	 * @return array|string $value The sanitized array (or string from the callback)
+	 */
+	public static function sanitize_posted_field( $value ) {
+		// Decode URLs
+		if ( is_string( $value ) && ( strstr( $value, 'http:' ) || strstr( $value, 'https:' ) ) ) {
+			$value = urldecode( $value );
+		}
+
+		// Santize value
+		$value = is_array( $value ) ? array_map( array( __CLASS__, 'sanitize_posted_field' ), $value ) : sanitize_text_field( stripslashes( trim( $value ) ) );
+
+		return $value;
+	}
+
+	/**
+	 * Get the value of submitted fields.
+	 *
+	 * @access protected
+	 * @param  string $key
+	 * @param  array $field
+	 * @since 1.0.0
+	 * @return array|string content of the submitted field
+	 */
+	protected static function get_posted_field( $key, $field ) {
+		return isset( $_POST[ $key ] ) ? self::sanitize_posted_field( $_POST[ $key ] ) : '';
+	}
+
+	/**
+	 * Validate the posted fields
+	 *
+	 * @return bool on success, WP_ERROR on failure
+	 */
+	protected static function validate_fields( $values ) {
+
+		foreach ( self::$fields as $group_key => $group_fields ) {
+			foreach ( $group_fields as $key => $field ) {
+				if ( $field['required'] && empty( $values[ $group_key ][ $key ] ) ) {
+					return new WP_Error( 'validation-error', sprintf( __( '%s is a required field' ), $field['label'] ) );
+				}
+			}
+		}
+
+		return apply_filters( 'wpum_password_form_validate_fields', true, self::$fields, $values );
+
+	}
+
+	/**
+	 * Validate username field.
+	 *
+	 * @access public
+	 * @since 1.0.0
+	 * @return void
+	 */
+	public static function validate_username( $passed, $fields, $values ) {
+
+		$username = $values['user'][ 'username_email' ];
+
+		if( is_email( $username ) && !email_exists( $username ) || !is_email( $username ) && !username_exists( $username ) )
+			return new WP_Error( 'username-validation-error', __( 'This user could not be found.' ) );
+
+		return $passed;
+
 	}
 
 	/**
@@ -78,6 +199,19 @@ class WPUM_Form_Password extends WPUM_Form {
 
 		// Get fields
 		self::get_password_fields();
+
+		// Get posted values
+		$values = self::get_posted_fields();
+
+		if ( empty( $_POST['wpum_submit_form'] ) ) {
+			return;
+		}
+
+		// Validate required
+		if ( is_wp_error( ( $return = self::validate_fields( $values ) ) ) ) {
+			self::add_error( $return->get_error_message() );
+			return;
+		}
 
 	}
 
@@ -93,11 +227,18 @@ class WPUM_Form_Password extends WPUM_Form {
 		// Get fields
 		self::get_password_fields();
 
+		// Show errors from fields
+		self::show_errors();
+
+		// Show confirmation messages
+		self::show_confirmations();
+
 		// Display template
 		get_wpum_template( 'password-form.php', 
 			array(
-				'atts' => $atts,
-				'form' => self::$form_name,
+				'atts'            => $atts,
+				'form'            => self::$form_name,
+				'user_fields'     => self::get_fields( 'user' ),
 				'password_fields' => self::get_fields( 'password' ),
 			)
 		);
--- templates/password-form.php
+++ templates/password-form.php
@@ -10,28 +10,51 @@
  * @since       1.0.0
  */
 ?>
-<div id="wpum-form-password-<?php echo $atts['form_id'];?>" class="wpum-password-form-wrapper" data-redirect="<?php echo $atts['redirect'];?>">
+<div id="wpum-form-password-<?php echo $atts['form_id'];?>" class="wpum-password-form-wrapper">
 
 	<?php do_action( 'wpum_before_password_form_template', $atts ); ?>
 
-	<form action="#" method="post" id="wpum-password-<?php echo $atts['form_id'];?>" class="wpum-default-registration-form" name="wpum-password-<?php echo $atts['form_id'];?>">
+	<p class="wpum-message wpum-info wpum-lost-psw-message">
+		<?php echo apply_filters( 'wpum_lost_password_message', __( 'Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.' ) ); ?>
+	</p>
+
+	<form action="#" method="post" id="wpum-password-<?php echo $atts['form_id'];?>" class="wpum-password-form" name="wpum-password-<?php echo $atts['form_id'];?>">
 
 		<?php do_action( 'wpum_before_inside_password_form_template', $atts ); ?>
 
-		<?php foreach ( $password_fields as $key => $field ) : ?>
-			<fieldset class="fieldset-<?php esc_attr_e( $key ); ?>">
-				<label for="<?php esc_attr_e( $key ); ?>"><?php echo $field['label']; ?></label>
-				<div class="field <?php echo $field['required'] ? 'required-field' : ''; ?>">
-					<?php get_wpum_template( 'form-fields/' . $field['type'] . '-field.php', array( 'key' => $key, 'field' => $field ) ); ?>
-				</div>
-			</fieldset>
-		<?php endforeach; ?>
+		<?php if( isset($_GET['password-reset']) ) : ?>
+
+			<!-- Start Password Replace Fields -->
+			<?php foreach ( $password_fields as $key => $field ) : ?>
+				<fieldset class="fieldset-<?php esc_attr_e( $key ); ?>">
+					<label for="<?php esc_attr_e( $key ); ?>"><?php echo $field['label']; ?></label>
+					<div class="field <?php echo $field['required'] ? 'required-field' : ''; ?>">
+						<?php get_wpum_template( 'form-fields/' . $field['type'] . '-field.php', array( 'key' => $key, 'field' => $field ) ); ?>
+					</div>
+				</fieldset>
+			<?php endforeach; ?>
+			<!-- End Password Replace Fields -->
+
+		<?php else : ?>
+
+			<!-- Start Password User Fields -->
+			<?php foreach ( $user_fields as $key => $field ) : ?>
+				<fieldset class="fieldset-<?php esc_attr_e( $key ); ?>">
+					<label for="<?php esc_attr_e( $key ); ?>"><?php echo $field['label']; ?></label>
+					<div class="field <?php echo $field['required'] ? 'required-field' : ''; ?>">
+						<?php get_wpum_template( 'form-fields/' . $field['type'] . '-field.php', array( 'key' => $key, 'field' => $field ) ); ?>
+					</div>
+				</fieldset>
+			<?php endforeach; ?>
+			<!-- End Password User Fields -->
+
+		<?php endif; ?>
 
 		<?php do_action( 'wpum_after_inside_password_form_template', $atts ); ?>
 
 		<?php wp_nonce_field( 'wpum-password-nonce', 'security' ); ?>
 
-		<p>
+		<p class="wpum-submit">
 			<input type="hidden" name="wpum_submit_form" value="<?php echo $form; ?>" />
 			<input type="submit" id="submit_wpum_password" name="submit_wpum_password" class="button" value="<?php _e('Reset Password'); ?>" />
 		</p>
