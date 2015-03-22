ad66724018aab73dbae103a9cd4046634e7a99da
Alessandro Tesoro <mythemesdepot@gmail.com>
2015-03-22 14:15:09 +0000

    restructured profile.php template #24

--- includes/template-actions.php
+++ includes/template-actions.php
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * @param string $login yes/no
  * @param string $register yes/no
  * @param string $password yes/no
- * @return mixed
+ * @return void
  */
 function wpum_add_links_to_forms( $login, $register, $password ) {
 
@@ -42,7 +42,7 @@ add_action( 'wpum_do_helper_links', 'wpum_add_links_to_forms', 10, 3 );
  * @since 1.0.0
  * @access public
  * @param array $atts Settings of the shortcode.
- * @return mixed
+ * @return void
  */
 function wpum_add_helper_links( $atts ) {
 
@@ -56,3 +56,88 @@ function wpum_add_helper_links( $atts ) {
 }
 add_action( 'wpum_after_password_form_template', 'wpum_add_helper_links', 10, 1 );
 add_action( 'wpum_after_register_form_template', 'wpum_add_helper_links', 10, 1 );
+
+if ( ! function_exists( 'wpum_profile_show_avatar' ) ) :
+/**
+ * Display user avatar in profile.php template.
+ * 
+ * @since 1.0.0
+ * @param object $user_data holds WP_User object
+ * @access public
+ * @return void
+ */
+function wpum_profile_show_avatar( $user_data ) {
+
+	$output = '<div class="wpum-avatar-img wpum_one_sixth">';
+		$output .= '<a href="' . wpum_get_user_profile_url( $user_data ) . '">' . get_avatar( $user_data->ID , 128 ) . '</a>';
+	$output .= '</div>';
+
+	echo $output;
+
+}
+add_action( 'wpum_main_profile_details', 'wpum_profile_show_avatar', 10 );
+endif;
+
+if ( ! function_exists( 'wpum_profile_show_user_details' ) ) :
+/**
+ * Adds a column - used to wrap other elements inside it.
+ * Used in profile.php template.
+ * 
+ * @since 1.0.0
+ * @param object $user_data holds WP_User object
+ * @access public
+ * @return void
+ */
+function wpum_profile_show_user_details( $user_data ) {
+
+	$output = '<div class="wpum-inner-details wpum_five_sixth last">';
+		$output .= do_action( 'wpum_profile_show_user_details', $user_data );
+	$output .= '</div>';
+
+	echo $output;
+
+}
+add_action( 'wpum_main_profile_details', 'wpum_profile_show_user_details', 10 );
+endif;
+
+if ( ! function_exists( 'wpum_profile_show_user_name' ) ) :
+/**
+ * Display user name in profile.php template.
+ * 
+ * @since 1.0.0
+ * @param object $user_data holds WP_User object
+ * @access public
+ * @return void
+ */
+function wpum_profile_show_user_name( $user_data ) {
+
+	$output = '<div class="wpum-user-display-name">';
+		$output .= '<a href="'. wpum_get_user_profile_url( $user_data ) .'">'. esc_attr( $user_data->display_name ) .'</a>';
+	$output .= '</div>';
+
+	echo $output;
+
+}
+add_action( 'wpum_profile_show_user_details', 'wpum_profile_show_user_name', 10 );
+endif;
+
+if ( ! function_exists( 'wpum_profile_show_user_description' ) ) :
+/**
+ * Display user name in profile.php template.
+ * 
+ * @since 1.0.0
+ * @param object $user_data holds WP_User object
+ * @access public
+ * @return void
+ */
+function wpum_profile_show_user_description( $user_data ) {
+
+	$output = '<div class="wpum-user-description">';
+		$output .= wpautop( esc_attr( get_user_meta( $user_data->ID, 'description', true) ), true );
+	$output .= '</div>';
+
+	echo $output;
+
+}
+add_action( 'wpum_profile_show_user_details', 'wpum_profile_show_user_description', 10 );
+endif;
---
+++ templates/profile.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * WPUM Template: Mini Profile.
+ * Displays a preview of the user profile.
+ *
+ * @package     wp-user-manager
+ * @copyright   Copyright (c) 2015, Alessandro Tesoro
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.0.0
+ */
+
+// Display error message if no user has been found.
+if( !is_object( $user_data ) ) {
+	get_wpum_template( 'profile-not-found.php' );
+	return;
+}
+
+do_action( "wpum_before_profile", $user_data );
+
+?>
+
+<!-- start profile -->
+<div class="wpum-single-profile" id="wpum-profile-<?php echo $user_data->ID;?>">
+
+	<?php do_action( "wpum_before_profile_details", $user_data ); ?>
+
+	<!-- Profile details wrapper -->
+	<div class="wpum-user-details">
+
+		<!-- First column -->
+		<div class="wpum_three_fourth wpum-main-profile-details">
+			<?php do_action( 'wpum_main_profile_details', $user_data ); ?>
+		</div>
+		<!-- end first column -->
+
+		<!-- last column -->
+		<div class="wpum_one_fourth last wpum-secondary-profile-details">
+			<?php do_action( 'wpum_secondary_profile_details', $user_data ); ?>
+		</div>
+		<!-- end last column -->
+
+		<div class="wpum-clearfix"></div>
+
+	</div>
+	<!-- end profile details wrapper -->
+
+	<?php do_action( "wpum_after_profile_details", $user_data ); ?>
+
+</div>
+<!-- end profile -->
+
+<?php do_action( "wpum_after_profile", $user_data ); ?>
