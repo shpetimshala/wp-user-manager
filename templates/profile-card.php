252bd42d70fc7da49ffc2c03ad2d3d5647a1b951
Alessandro Tesoro <mythemesdepot@gmail.com>
2015-03-31 14:19:57 +0200

    completed profile card template layout #24

--- templates/profile-card.php
+++ templates/profile-card.php
@@ -8,24 +8,36 @@
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       1.0.0
  */
+
+// Return different template if set.
+if( $template ) {
+	get_wpum_template( "profile-card-{$template}.php", array( 
+			'user_data'  => $user_data,
+			'template'   => $template,
+			'wrapper_id' => $wrapper_id
+		) 
+	);
+	return;
+}
 ?>
 
 <div id="wpum-profile-card<?php echo $wrapper_id; ?>" class="wpum-profile-card">
 
-	<div class="wpum-profile-img wpum_one_sixth">
-		<?php echo wpum_profile_avatar( $user_data ); ?>
+	<div class="wpum-profile-img">
+		<?php echo wpum_profile_avatar( $user_data, true, 100 ); ?>
 	</div>
 
-	<div class="wpum-card-details wpum_five_sixth last">
+	<div class="wpum-card-details">
 		
-		<?php echo wpum_profile_display_name( $user_data ); ?>
-		<a href=""><?php _e('View Profile');?></a>
-		<a href=""><?php _e('Send Email');?></a>
+		<h4 class="wpum-card-name"><?php echo wpum_profile_display_name( $user_data, true ); ?></h4>
+
+		<?php echo wpautop( $user_data->user_description ); ?>
+
+		<a href="<?php echo wpum_get_user_profile_url( $user_data ); ?>" class="wpum-card-button"><?php _e('View Profile');?></a>
+		<a href="mailto:<?php echo antispambot( $user_data->user_email );?>" class="wpum-card-button"><?php _e('Send Email');?></a>
 
 		<?php do_action( 'wpum_profile_card_details', $user_data ); ?>
 
 	</div>
-
-	<div class="wpum-clearfix"></div>
 
 </div>
