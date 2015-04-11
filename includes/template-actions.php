87f8e1e10c2f405f91aed6a5365a257a5879fd34
Alessandro Tesoro <mythemesdepot@gmail.com>
2015-04-11 14:57:22 +0200

    fixed bug with pagination on directory when sorting is selected #24

--- includes/template-actions.php
+++ includes/template-actions.php
@@ -92,7 +92,7 @@ function wpum_user_directory_pagination( $directory_args ) {
 
 	echo paginate_links( array(
 				'base'      => get_pagenum_link(1) . '%_%',
-				'format'    => '?paged=%#%',
+				'format'    => isset( $_GET['sort'] ) ? '&paged=%#%' : '?paged=%#%',
 				'current'   => $directory_args['paged'],  
 				'total'     => $directory_args['total_pages'],
 				'prev_text' => __('Previous page'),
