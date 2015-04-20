dad4203790a5e5613835e5decd16745886234fa2
Alessandro Tesoro <mythemesdepot@gmail.com>
2015-04-20 13:51:34 +0100

    updated navigation in fields editor #64

--- includes/class-wpum-custom-fields-editor.php
+++ includes/class-wpum-custom-fields-editor.php
@@ -26,6 +26,14 @@ class WPUM_Custom_Fields_Editor {
 	const Hook = 'users_page_wpum-custom-fields-editor';
 
 	/**
+	 * Links of the editor navbar
+	 *
+	 * @var object
+	 * @since 1.0.0
+	 */
+	public static $nav_links;
+
+	/**
 	 * __construct function.
 	 *
 	 * @access public
@@ -36,6 +44,18 @@ class WPUM_Custom_Fields_Editor {
 		add_action( 'load-'.self::Hook, array( $this, 'add_screen_meta_boxes' ) );
 		add_action( 'add_meta_boxes_'.self::Hook, array( $this, 'add_meta_box' ) );
 		add_action( 'admin_footer-'.self::Hook, array( $this, 'print_script_in_footer' ) );
+
+		// Navbar links
+		self::$nav_links = array(
+			array(
+				'type' => 'registration',
+				'title' => __('Registration fields')
+			),
+			array(
+				'type' => 'profile',
+				'title' => __('Profile fields')
+			),
+		);
 
 	}
 
@@ -59,12 +79,18 @@ class WPUM_Custom_Fields_Editor {
 
 			<div id="nav-menus-frame">
 
+				<!-- Sidebar -->
 				<div id="menu-settings-column" class="metabox-holder">
 					
 					<div class="clear"></div>
 
 					<?php do_accordion_sections( self::Hook, 'side', null ); ?>
 						
+				</div>
+				<!-- End Sidebar -->
+
+				<div id="menu-management-liquid">
+				
 				</div>
 
 			</div>
@@ -100,10 +126,21 @@ class WPUM_Custom_Fields_Editor {
 	 */
 	public static function navbar() {
 		
+		// Define base url
+		$url = admin_url( 'users.php?page=wpum-custom-fields-editor' );
+
 		$output = '<div class="wp-filter">';
 			$output .= '<ul class="filter-links">';
-				$output .= '<li><a href="" class=" current">'. __('Registration Fields') .'</a></li>';
-				$output .= '<li><a href="" class="">'. __('Profile Fields') .'</a></li>';
+				
+				foreach ( self::$nav_links as $link ) {
+					$link_url = add_query_arg( array( 'editor' => $link['type'] ), $url );
+					if( isset( $_GET['editor'] ) && $_GET['editor'] == $link['type'] || !isset( $_GET['editor'] ) && $link['type'] == 'registration' ) :
+						$output .= '<li><a href="'.$link_url.'" class="current">'. $link['title'] .'</a></li>';
+					else : 
+						$output .= '<li><a href="'.$link_url.'">'. $link['title'] .'</a></li>';
+					endif;
+				}
+
 			$output .= '</ul>';
 		$output .= '</div>';
 
@@ -118,25 +155,23 @@ class WPUM_Custom_Fields_Editor {
 	 */
 	public function add_meta_box() {
 
-		add_meta_box(
-			'myplugin_sectionid',
-			__( 'My Post Section Title', 'myplugin_textdomain' ),
-			array( $this, 'test' ),
-			self::Hook,
-			'side'
-		);
-
-		add_meta_box(
-			'myplugin_sectionid2',
-			__( 'T1', 'myplugin_textdomain' ),
-			array( $this, 'test' ),
-			self::Hook,
-			'side'
-		);
+		add_meta_box( 'wpum_fields_editor_help', __( 'How it works' ), array( $this, 'help_text' ), self::Hook, 'side' );
 
 	}
 
-	function test() {
+	/**
+	 * Content of the first metabox.
+	 *
+	 * @access public
+	 * @return mixed content of the "how it works" metabox.
+	 */
+	public function help_text() {
+
+		$output = '<p>';
+			$output .= sprintf( __('Click and drag the %s button or the field order number to change the order of the fields.'), '<span class="dashicons dashicons-sort"></span>');
+		$output .= '</p>';
+
+		echo $output;
 
 	}
 
