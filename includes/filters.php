6948cad2dcdd372ee868a457906ec4d7866cf308
Alessandro Tesoro <mythemesdepot@gmail.com>
2015-04-28 12:55:59 +0100

    fixed get_avatar filter logic

--- includes/filters.php
+++ includes/filters.php
@@ -291,21 +291,24 @@ add_filter( 'wpum_user_directory_query', 'wpum_directory_pre_set_amount', 11, 2 
 function wpum_get_avatar( $avatar, $id_or_email, $size, $default, $alt ) {
 
 	$safe_alt = esc_attr( $alt );
+	$custom_avatar = false;
 
-	if ( is_numeric( $id_or_email ) ) {
+	if( is_object( $id_or_email ) ) {
+
+		$comment_email = $id_or_email->comment_author_email;
+
+		$user = get_user_by( 'email', $comment_email );
+		
+		if( $user ) {
+			$custom_avatar = get_user_meta( $user->ID , 'current_user_avatar', true );
+		}
+
+	} elseif ( is_email( $id_or_email ) && email_exists( $id_or_email ) || is_numeric( $id_or_email ) ) {
 		$custom_avatar = get_user_meta( $id_or_email, 'current_user_avatar', true );
-			if ( !empty( $custom_avatar ) ) {
-				$avatar = "<img alt='{$safe_alt}' src='{$custom_avatar}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
-		}
-	} else if ( is_email( $id_or_email ) && email_exists( $id_or_email ) ) {
-
-		$get_user = get_user_by( 'email', $id_or_email );
-
-		$custom_avatar = get_user_meta( $get_user->ID , 'current_user_avatar', true );
-			if ( !empty( $custom_avatar ) ) {
-				$avatar = "<img alt='{$safe_alt}' src='{$custom_avatar}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
-		}
-
+	}
+	
+	if ( !empty( $custom_avatar ) ) {
+		$avatar = "<img alt='{$safe_alt}' src='{$custom_avatar}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
 	}
 
 	return $avatar;
