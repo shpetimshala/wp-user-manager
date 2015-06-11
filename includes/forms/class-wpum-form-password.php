1e8f74f288e582b571452b7ecd3dc6590d7ee61e
Alessandro Tesoro <mythemesdepot@gmail.com>
2015-06-11 12:05:56 +0100

    cleaned up password recovery form

--- includes/forms/class-wpum-form-password.php
+++ includes/forms/class-wpum-form-password.php
@@ -33,10 +33,10 @@ class WPUM_Form_Password extends WPUM_Form {
 
 		// Validate username field
 		if( !isset( $_GET['password-reset'] ) )
-			add_filter( 'wpum_password_recovery_validation', array( __CLASS__, 'validate_username' ), 10, 3 );
+			add_filter( 'wpum/form/validate=password', array( __CLASS__, 'validate_username' ), 10, 3 );
 
 		if( isset( $_GET['password-reset'] ) )
-			add_filter( 'wpum_password_recovery_validation', array( __CLASS__, 'validate_passwords' ), 10, 3 );
+			add_filter( 'wpum/form/validate=password', array( __CLASS__, 'validate_passwords' ), 10, 3 );
 
 	}
 
@@ -88,131 +88,6 @@ class WPUM_Form_Password extends WPUM_Form {
 			unset( self::$fields['user']['username_email'] );
 		endif;
 
-
-	}
-
-	/**
-	 * Get submitted fields values.
-	 *
-	 * @access public
-	 * @since 1.0.0
-	 * @return $values array of data from the fields.
-	 */
-	protected static function get_posted_fields() {
-
-		// Get fields
-		self::get_password_fields();
-
-		$values = array();
-
-		foreach ( self::$fields as $group_key => $group_fields ) {
-			foreach ( $group_fields as $key => $field ) {
-				// Get the value
-				$field_type = str_replace( '-', '_', $field['type'] );
-
-				if ( method_exists( __CLASS__, "get_posted_{$field_type}_field" ) ) {
-					$values[ $group_key ][ $key ] = call_user_func( __CLASS__ . "::get_posted_{$field_type}_field", $key, $field );
-				} else {
-					$values[ $group_key ][ $key ] = self::get_posted_field( $key, $field );
-				}
-
-				// Set fields value
-				self::$fields[ $group_key ][ $key ]['value'] = $values[ $group_key ][ $key ];
-			}
-		}
-
-		return $values;
-	}
-
-	/**
-	 * Goes through fields and sanitizes them.
-	 *
-	 * @access public
-	 * @param array|string $value The array or string to be sanitized.
-	 * @since 1.0.0
-	 * @return array|string $value The sanitized array (or string from the callback)
-	 */
-	public static function sanitize_posted_field( $value ) {
-		// Decode URLs
-		if ( is_string( $value ) && ( strstr( $value, 'http:' ) || strstr( $value, 'https:' ) ) ) {
-			$value = urldecode( $value );
-		}
-
-		// Santize value
-		$value = is_array( $value ) ? array_map( array( __CLASS__, 'sanitize_posted_field' ), $value ) : sanitize_text_field( stripslashes( trim( $value ) ) );
-
-		return $value;
-	}
-
-	/**
-	 * Get the value of submitted fields.
-	 *
-	 * @access protected
-	 * @param  string $key
-	 * @param  array $field
-	 * @since 1.0.0
-	 * @return array|string content of the submitted field
-	 */
-	protected static function get_posted_field( $key, $field ) {
-		return isset( $_POST[ $key ] ) ? self::sanitize_posted_field( $_POST[ $key ] ) : '';
-	}
-
-	/**
-	 * Get the value of a posted multiselect field
-	 * @param  string $key
-	 * @param  array $field
-	 * @return array
-	 */
-	protected static function get_posted_multiselect_field( $key, $field ) {
-		return isset( $_POST[ $key ] ) ? array_map( 'sanitize_text_field', $_POST[ $key ] ) : array();
-	}
-
-	/**
-	 * Get the value of a posted number field
-	 * @param  string $key
-	 * @param  array $field
-	 * @return string
-	 */
-	protected static function get_posted_number_field( $key, $field ) {
-		return isset( $_POST[ $key ] ) ? intval( $_POST[ $key ] ) : '';
-	}
-
-	/**
-	 * Get the value of a posted textarea field
-	 * @param  string $key
-	 * @param  array $field
-	 * @return string
-	 */
-	protected static function get_posted_textarea_field( $key, $field ) {
-		return isset( $_POST[ $key ] ) ? wp_kses_post( trim( stripslashes( $_POST[ $key ] ) ) ) : '';
-	}
-
-	/**
-	 * Get the value of a posted textarea field
-	 * @param  string $key
-	 * @param  array $field
-	 * @return string
-	 */
-	protected static function get_posted_wp_editor_field( $key, $field ) {
-		return self::get_posted_textarea_field( $key, $field );
-	}
-
-	/**
-	 * Validate the posted fields
-	 *
-	 * @return bool on success, WP_ERROR on failure
-	 */
-	protected static function validate_fields( $values ) {
-
-		foreach ( self::$fields as $group_key => $group_fields ) {
-			foreach ( $group_fields as $key => $field ) {
-				if ( $field['required'] && empty( $values[ $group_key ][ $key ] ) ) {
-					return new WP_Error( 'validation-error', sprintf( __( '%s is a required field' ), $field['label'] ) );
-				}
-			}
-		}
-
-		return apply_filters( 'wpum_password_recovery_validation', true, self::$fields, $values );
 
 	}
 
@@ -278,7 +153,7 @@ class WPUM_Form_Password extends WPUM_Form {
 		}
 
 		// Validate required
-		if ( is_wp_error( ( $return = self::validate_fields( $values ) ) ) ) {
+		if ( is_wp_error( ( $return = self::validate_fields( $values, self::$form_name ) ) ) ) {
 			self::add_error( $return->get_error_message() );
 			return;
 		}
