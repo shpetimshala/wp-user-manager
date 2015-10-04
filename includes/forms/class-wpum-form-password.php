8a9d93ca059657814421da6c3cc1a215e680a6d4
alessandrotesoro <mythemesdepot@gmail.com>
2015-10-04 17:52:33 +0100

    feat: added password meter to password recovery form

--- includes/forms/class-wpum-form-password.php
+++ includes/forms/class-wpum-form-password.php
@@ -38,6 +38,10 @@ class WPUM_Form_Password extends WPUM_Form {
 		if( isset( $_GET['password-reset'] ) )
 			add_filter( 'wpum/form/validate=password', array( __CLASS__, 'validate_passwords' ), 10, 3 );
 
+		// Add password meter field
+		if( wpum_get_option('display_password_meter_registration') )
+			add_action( 'wpum_after_inside_password_form_template', 'wpum_psw_indicator' );
+
 	}
 
 	/**
