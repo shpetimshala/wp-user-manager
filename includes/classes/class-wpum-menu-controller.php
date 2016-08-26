6d095425abeb826a13814c36f479890d7370bd22
alessandrotesoro <mythemesdepot@gmail.com>
2016-08-26 19:53:34 +0200

    hook for custom fields is triggering fine #229

--- includes/classes/class-wpum-menu-controller.php
+++ includes/classes/class-wpum-menu-controller.php
@@ -18,25 +18,27 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  */
 class WPUM_Menu_Controller {
 
-  public function init() {
+	public function init() {
 
-    // Change admin walker.
-    add_filter( 'wp_edit_nav_menu_walker', array( $this, 'edit_nav_menu_walker' ) );
+		// Change admin walker.
+		add_filter( 'wp_edit_nav_menu_walker', array( $this, 'edit_nav_menu_walker' ) );
 
-    // Add fields via hook.
-    add_action( 'wp_nav_menu_item_custom_fields', array( $this, 'add_custom_fields' ), 10, 4 );
+		// Add fields via hook.
+		add_action( 'wp_nav_menu_item_custom_fields', array( $this, 'add_custom_fields' ), 10, 4 );
 
-  }
+	}
 
-  public function edit_nav_menu_walker( $walker ) {
+	public function edit_nav_menu_walker( $walker ) {
 
-    return 'Walker_WPUM_Nav_Menu_Roles_Controller';
+		return 'Walker_WPUM_Nav_Menu_Roles_Controller';
 
-  }
+	}
 
-  public function add_custom_fields( $item_id, $item, $depth, $args ) {
+	public function add_custom_fields( $item_id, $item, $depth, $args ) {
 
-  }
+		echo "test";
+
+	}
 
 
 }
